e8b11e52aced0a9269b049260f18815a70f4b16b
Brandon Pokorny <pokorny3@illinois.edu>
2020-11-22 20:23:05 -0600

    Added translation and scoring

---
+++ spDecoder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import argparse #Arguments and flags
+import sentencepiece as spm #Subword segmentation
+
+
+
+def main(args):
+	if args.input == "" or args.output == "" or args.model == "":
+		print("Incorrect usage. Please see spDecoder.py -h")
+		exit(1)
+	model = spm.SentencePieceProcessor(model_file=args.model)
+	inputFile = open(args.input, "r")
+	outputFile = open(args.output, "w+")
+	while 1:
+		line = inputFile.readline().replace("\n", "")
+		if line == "":
+			break #EOF reached
+		line = model.decode(line)
+		for token in line:
+			outputFile.write(str(token) + " ")
+		outputFile.write("\n")
+	inputFile.close()
+	outputFile.close()
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Python script wrapper for spm_decode command in the terminal")
+	#parser.add_argument('-t', dest='test', type=bool, default=False, help='This is a test argument, ignore it')
+	parser.add_argument('--input', dest='input', type=str, default="", help="Data for decoding")
+	parser.add_argument('--output', dest='output', type=str, default="", help="Output for decoded data")
+	parser.add_argument('--model', dest='model', type=str, default="", help="The trained model to use")
+	args = parser.parse_args()
+	main(args)
